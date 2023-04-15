e793ebdc34049c87c9bb84f503c26ff9fd653121
Bishal <adbishal569@gmail.com>
2023-04-15 15:59:23 +0545

    Main file complet

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-#This is by Bishal Git hub
-#This is written by meow
+import add_work
+import sub_work
+import div_work
+import multi_work
+a = 50
+b = 2
+total_add = add_work.add(a, b)
+total_sub = sub_work(a, b)
+total_mul = multi_work(a, b)
+total_div = div_work(a, b)
+
+print(f"Total sum is {total_add}. Total Sub is {total_sub}. Total div is {total_div}. Total mul is {total_mul} ")
