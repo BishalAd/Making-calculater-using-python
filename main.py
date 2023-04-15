32c2f7f065004f7bec2c72a5b258651e427a499f
Bishal <adbishal569@gmail.com>
2023-04-15 16:02:46 +0545

    silly mistake solve

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import multi_work
 a = 50
 b = 2
 total_add = add_work.add(a, b)
-total_sub = sub_work(a, b)
-total_mul = multi_work(a, b)
-total_div = div_work(a, b)
+total_sub = sub_work.sub(a, b)
+total_mul = multi_work.mul(a, b)
+total_div = div_work.div(a, b)
 
 print(f"Total sum is {total_add}. Total Sub is {total_sub}. Total div is {total_div}. Total mul is {total_mul} ")
